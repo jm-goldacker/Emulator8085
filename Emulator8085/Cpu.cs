a3dd396e97fcc19e6ab4b1719f5b0202856c9389
Johann Maximilian Goldacker <johann.goldacker@student.uni-halle.de>
2021-06-20 23:19:51 +0200

    all MOV instructions

--- Emulator8085/Cpu.cs
+++ Emulator8085/Cpu.cs
@@ -127,10 +127,224 @@ namespace Emulator8085
                     TryMoveNextByteInMemoryTo(ref L);
                     break;
 
-                case InstructionSet.MOV_A_B:
-                    MoveFromTo(B, ref A);
+                case InstructionSet.MOV_A_A:
+                    MoveToFrom(ref A, A);
                     break;
 
+                case InstructionSet.MOV_A_B:
+                    MoveToFrom(ref A, B);
+                    break;
+
+                case InstructionSet.MOV_A_C:
+                    MoveToFrom(ref A, C);
+                    break;
+
+                case InstructionSet.MOV_A_D:
+                    MoveToFrom(ref A, D);
+                    break;
+
+                case InstructionSet.MOV_A_E:
+                    MoveToFrom(ref A, E);
+                    break;
+
+                case InstructionSet.MOV_A_H:
+                    MoveToFrom(ref A, H);
+                    break;
+
+                case InstructionSet.MOV_A_L:
+                    MoveToFrom(ref A, L);
+                    break;
+
+                case InstructionSet.MOV_B_A:
+                    MoveToFrom(ref B, A);
+                    break;
+
+                case InstructionSet.MOV_B_B:
+                    MoveToFrom(ref B, B);
+                    break;
+
+                case InstructionSet.MOV_B_C:
+                    MoveToFrom(ref B, C);
+                    break;
+
+                case InstructionSet.MOV_B_D:
+                    MoveToFrom(ref B, D);
+                    break;
+
+                case InstructionSet.MOV_B_E:
+                    MoveToFrom(ref B, E);
+                    break;
+
+                case InstructionSet.MOV_B_H:
+                    MoveToFrom(ref B, H);
+                    break;
+
+                case InstructionSet.MOV_B_L:
+                    MoveToFrom(ref B, L);
+                    break;
+
+                case InstructionSet.MOV_C_A:
+                    MoveToFrom(ref C, A);
+                    break;
+
+                case InstructionSet.MOV_C_B:
+                    MoveToFrom(ref C, B);
+                    break;
+
+                case InstructionSet.MOV_C_C:
+                    MoveToFrom(ref C, C);
+                    break;
+
+                case InstructionSet.MOV_C_D:
+                    MoveToFrom(ref C, D);
+                    break;
+
+                case InstructionSet.MOV_C_E:
+                    MoveToFrom(ref C, E);
+                    break;
+
+                case InstructionSet.MOV_C_H:
+                    MoveToFrom(ref C, H);
+                    break;
+
+                case InstructionSet.MOV_C_L:
+                    MoveToFrom(ref C, L);
+                    break;
+
+                case InstructionSet.MOV_D_A:
+                    MoveToFrom(ref D, A);
+                    break;
+
+                case InstructionSet.MOV_D_B:
+                    MoveToFrom(ref D, B);
+                    break;
+
+                case InstructionSet.MOV_D_C:
+                    MoveToFrom(ref D, C);
+                    break;
+
+                case InstructionSet.MOV_D_D:
+                    MoveToFrom(ref D, D);
+                    break;
+
+                case InstructionSet.MOV_D_E:
+                    MoveToFrom(ref D, E);
+                    break;
+
+                case InstructionSet.MOV_D_H:
+                    MoveToFrom(ref D, H);
+                    break;
+
+                case InstructionSet.MOV_D_L:
+                    MoveToFrom(ref D, L);
+                    break;
+
+                case InstructionSet.MOV_E_A:
+                    MoveToFrom(ref E, A);
+                    break;
+
+                case InstructionSet.MOV_E_B:
+                    MoveToFrom(ref E, B);
+                    break;
+
+                case InstructionSet.MOV_E_C:
+                    MoveToFrom(ref E, C);
+                    break;
+
+                case InstructionSet.MOV_E_D:
+                    MoveToFrom(ref E, D);
+                    break;
+
+                case InstructionSet.MOV_E_E:
+                    MoveToFrom(ref E, E);
+                    break;
+
+                case InstructionSet.MOV_E_H:
+                    MoveToFrom(ref E, H);
+                    break;
+
+                case InstructionSet.MOV_E_L:
+                    MoveToFrom(ref E, L);
+                    break;
+
+                case InstructionSet.MOV_H_A:
+                    MoveToFrom(ref H, A);
+                    break;
+
+                case InstructionSet.MOV_H_B:
+                    MoveToFrom(ref H, B);
+                    break;
+
+                case InstructionSet.MOV_H_C:
+                    MoveToFrom(ref H, C);
+                    break;
+
+                case InstructionSet.MOV_H_D:
+                    MoveToFrom(ref H, D);
+                    break;
+
+                case InstructionSet.MOV_H_E:
+                    MoveToFrom(ref H, E);
+                    break;
+
+                case InstructionSet.MOV_H_H:
+                    MoveToFrom(ref H, H);
+                    break;
+
+                case InstructionSet.MOV_H_L:
+                    MoveToFrom(ref H, L);
+                    break;
+
+                case InstructionSet.MOV_L_A:
+                    MoveToFrom(ref L, A);
+                    break;
+
+                case InstructionSet.MOV_L_B:
+                    MoveToFrom(ref L, B);
+                    break;
+
+                case InstructionSet.MOV_L_C:
+                    MoveToFrom(ref L, C);
+                    break;
+
+                case InstructionSet.MOV_L_D:
+                    MoveToFrom(ref L, D);
+                    break;
+
+                case InstructionSet.MOV_L_E:
+                    MoveToFrom(ref L, E);
+                    break;
+
+                case InstructionSet.MOV_L_H:
+                    MoveToFrom(ref L, H);
+                    break;
+
+                case InstructionSet.MOV_L_L:
+                    MoveToFrom(ref L, L);
+                    break;
+
+                    /*
+                case InstructionSet.MOV_M_A:
+                    break;
+
+                case InstructionSet.MOV_M_B:
+                    break;
+
+                case InstructionSet.MOV_M_C:
+                    break;
+
+                case InstructionSet.MOV_M_D:
+                    break;
+
+                case InstructionSet.MOV_M_E:
+                    break;
+
+                case InstructionSet.MOV_M_H:
+                    break;
+
+                case InstructionSet.MOV_M_L:
+                    break;
+                    */
                 default:
                     Console.WriteLine("Unknown instruction. Stopping programm!");
                     return false;
@@ -156,7 +370,7 @@ namespace Emulator8085
             return true;
         }
 
-        private void MoveFromTo(byte sourceRegister, ref byte destinationRegister)
+        private void MoveToFrom(ref byte destinationRegister, byte sourceRegister)
         {
             destinationRegister = sourceRegister;
         }
--- Emulator8085/InstructionSet.cs
+++ Emulator8085/InstructionSet.cs
@@ -46,8 +46,285 @@
         public const byte MVI_L = 0x2E;
 
         /// <summary>
+        /// Moves byte in register A to register A
+        /// </summary>
+        public const byte MOV_A_A = 0x7F;
+
+        /// <summary>
         /// Moves byte in register B to register A
         /// </summary>
         public const byte MOV_A_B = 0x78;
+
+        /// <summary>
+        /// Moves byte in register C to register A
+        /// </summary>
+        public const byte MOV_A_C = 0x79;
+
+        /// <summary>
+        /// Moves byte in register D to register A
+        /// </summary>
+        public const byte MOV_A_D = 0x7A;
+
+        /// <summary>
+        /// Moves byte in register E to register A
+        /// </summary>
+        public const byte MOV_A_E = 0x7B;
+
+        /// <summary>
+        /// Moves byte in register H to register A
+        /// </summary>
+        public const byte MOV_A_H = 0x7C;
+
+        /// <summary>
+        /// Moves byte in register L to register A
+        /// </summary>
+        public const byte MOV_A_L = 0x7D;
+
+        /// <summary>
+        /// Moves byte in register A to register B
+        /// </summary>
+        public const byte MOV_B_A = 0x47;
+
+        /// <summary>
+        /// Moves byte in register B to register B
+        /// </summary>
+        public const byte MOV_B_B = 0x40;
+
+        /// <summary>
+        /// Moves byte in register C to register B
+        /// </summary>
+        public const byte MOV_B_C = 0x41;
+
+        /// <summary>
+        /// Moves byte in register D to register B
+        /// </summary>
+        public const byte MOV_B_D = 0x42;
+
+        /// <summary>
+        /// Moves byte in register E to register B
+        /// </summary>
+        public const byte MOV_B_E = 0x43;
+
+        /// <summary>
+        /// Moves byte in register H to register B
+        /// </summary>
+        public const byte MOV_B_H = 0x44;
+
+        /// <summary>
+        /// Moves byte in register L to register B
+        /// </summary>
+        public const byte MOV_B_L = 0x45;
+
+        /// <summary>
+        /// Moves byte in register A to register C
+        /// </summary>
+        public const byte MOV_C_A = 0x4F;
+
+        /// <summary>
+        /// Moves byte in register B to register C
+        /// </summary>
+        public const byte MOV_C_B = 0x48;
+
+        /// <summary>
+        /// Moves byte in register C to register C
+        /// </summary>
+        public const byte MOV_C_C = 0x49;
+
+        /// <summary>
+        /// Moves byte in register D to register C
+        /// </summary>
+        public const byte MOV_C_D = 0x4A;
+
+        /// <summary>
+        /// Moves byte in register E to register C
+        /// </summary>
+        public const byte MOV_C_E = 0x4B;
+
+        /// <summary>
+        /// Moves byte in register H to register C
+        /// </summary>
+        public const byte MOV_C_H = 0x4C;
+
+        /// <summary>
+        /// Moves byte in register L to register C
+        /// </summary>
+        public const byte MOV_C_L = 0x4D;
+
+        /// <summary>
+        /// Moves byte in register A to register D
+        /// </summary>
+        public const byte MOV_D_A = 0x57;
+
+        /// <summary>
+        /// Moves byte in register B to register D
+        /// </summary>
+        public const byte MOV_D_B = 0x50;
+
+        /// <summary>
+        /// Moves byte in register C to register D
+        /// </summary>
+        public const byte MOV_D_C = 0x51;
+
+        /// <summary>
+        /// Moves byte in register D to register D
+        /// </summary>
+        public const byte MOV_D_D = 0x52;
+
+        /// <summary>
+        /// Moves byte in register E to register D
+        /// </summary>
+        public const byte MOV_D_E = 0x53;
+
+        /// <summary>
+        /// Moves byte in register H to register D
+        /// </summary>
+        public const byte MOV_D_H = 0x54;
+
+        /// <summary>
+        /// Moves byte in register L to register D
+        /// </summary>
+        public const byte MOV_D_L = 0x55;
+
+        /// <summary>
+        /// Moves byte in register A to register E
+        /// </summary>
+        public const byte MOV_E_A = 0x5F;
+
+        /// <summary>
+        /// Moves byte in register B to register E
+        /// </summary>
+        public const byte MOV_E_B = 0x58;
+
+        /// <summary>
+        /// Moves byte in register C to register E
+        /// </summary>
+        public const byte MOV_E_C = 0x59;
+
+        /// <summary>
+        /// Moves byte in register D to register E
+        /// </summary>
+        public const byte MOV_E_D = 0x5A;
+
+        /// <summary>
+        /// Moves byte in register E to register E
+        /// </summary>
+        public const byte MOV_E_E = 0x5B;
+
+        /// <summary>
+        /// Moves byte in register H to register E
+        /// </summary>
+        public const byte MOV_E_H = 0x5C;
+
+        /// <summary>
+        /// Moves byte in register L to register E
+        /// </summary>
+        public const byte MOV_E_L = 0x5D;
+
+        /// <summary>
+        /// Moves byte in register A to register H
+        /// </summary>
+        public const byte MOV_H_A = 0x67;
+
+        /// <summary>
+        /// Moves byte in register B to register H
+        /// </summary>
+        public const byte MOV_H_B = 0x60;
+
+        /// <summary>
+        /// Moves byte in register C to register H
+        /// </summary>
+        public const byte MOV_H_C = 0x61;
+
+        /// <summary>
+        /// Moves byte in register D to register H
+        /// </summary>
+        public const byte MOV_H_D = 0x62;
+
+        /// <summary>
+        /// Moves byte in register E to register H
+        /// </summary>
+        public const byte MOV_H_E = 0x63;
+
+        /// <summary>
+        /// Moves byte in register H to register H
+        /// </summary>
+        public const byte MOV_H_H = 0x64;
+
+        /// <summary>
+        /// Moves byte in register L to register H
+        /// </summary>
+        public const byte MOV_H_L = 0x65;
+
+        /// <summary>
+        /// Moves byte in register A to register L
+        /// </summary>
+        public const byte MOV_L_A = 0x6F;
+
+        /// <summary>
+        /// Moves byte in register B to register L
+        /// </summary>
+        public const byte MOV_L_B = 0x68;
+
+        /// <summary>
+        /// Moves byte in register C to register L
+        /// </summary>
+        public const byte MOV_L_C = 0x69;
+
+        /// <summary>
+        /// Moves byte in register D to register L
+        /// </summary>
+        public const byte MOV_L_D = 0x6A;
+
+        /// <summary>
+        /// Moves byte in register E to register L
+        /// </summary>
+        public const byte MOV_L_E = 0x6B;
+
+        /// <summary>
+        /// Moves byte in register H to register L
+        /// </summary>
+        public const byte MOV_L_H = 0x6C;
+
+        /// <summary>
+        /// Moves byte in register L to register L
+        /// </summary>
+        public const byte MOV_L_L = 0x6D;
+
+        /// <summary>
+        /// Writes content of register A to RAM adress specified in H and L
+        /// </summary>
+        public const byte MOV_M_A = 0x77;
+
+        /// <summary>
+        /// Writes content of register B to RAM adress specified in H and L
+        /// </summary>
+        public const byte MOV_M_B = 0x70;
+
+        /// <summary>
+        /// Writes content of register C to RAM adress specified in H and L
+        /// </summary>
+        public const byte MOV_M_C = 0x71;
+
+        /// <summary>
+        /// Writes content of register D to RAM adress specified in H and L
+        /// </summary>
+        public const byte MOV_M_D = 0x72;
+
+        /// <summary>
+        /// Writes content of register E to RAM adress specified in H and L
+        /// </summary>
+        public const byte MOV_M_E = 0x73;
+
+        /// <summary>
+        /// Writes content of register H to RAM adress specified in H and L
+        /// </summary>
+        public const byte MOV_M_H = 0x74;
+
+        /// <summary>
+        /// Writes content of register L to RAM adress specified in H and L
+        /// </summary>
+        public const byte MOV_M_L = 0x75;
+
+
     }
 }
